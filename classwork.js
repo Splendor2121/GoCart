6fca209496a867bc73421dfadf9fe420f360937a
Splendor2121 <161147854+Splendor2121@users.noreply.github.com>
2024-04-25 14:36:33 +0100

    Add files via upload
    I created a GoCart website

---
+++ classwork.js
@@ -0,0 +1,35 @@
+var mToggle = document.getElementById("menu-toggle")
+var menu = document.getElementById("menu")
+const slides = document.getElementsByClassName("slide")
+
+const active = () =>{
+    menu.classList.toggle("active")
+}
+mToggle.addEventListener("click", active)
+
+// CODE FOR SLIDER
+
+let slideIndex = 0
+
+const hide = () =>{
+    for(var i = 0; i < slides.length; i++){
+        slides[i].style.display = "none"
+    }
+}
+
+const showSlides = () =>{
+    hide()
+
+    slideIndex++
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block"
+    setTimeout(showSlides, 2000)
+}
+showSlides()
+
+//CODE FOR BACK TO TOP
+const backToTop = () =>{
+    document.documentElement.scrollTop = 0
+}
